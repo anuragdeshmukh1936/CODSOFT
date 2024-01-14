0adb99da869e9da87c11c8e22e0ee55a2becff67
anuragdeshmukh1936 <156404731+anuragdeshmukh1936@users.noreply.github.com>
2024-01-14 22:03:07 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    double num1, num2;
+    char op;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
+    switch(op)
+    {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+
+        case '/':
+            cout << num1 << " / " << num2 << " = " << num1 / num2;
+            break;
+
+        default:
+      
+            cout << "Invalid operator";
+            break;
+    }
+
+    return 0;
+}
